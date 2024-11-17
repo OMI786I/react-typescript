7c149399238ef8cafc02f2edbd18deccfe00f979
OMI786I <sohrawardy1998@gmail.com>
2024-11-17 11:48:18 +0600

    children props

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ function App() {
     <main>
       <h2>React & Typescript</h2>
       <Component id={2} name="Omi" />
+      <Component id={9} name="jahid">
+        <h1>Hellow world</h1>
+      </Component>
     </main>
   );
 }
--- src/starter/02-props/index.tsx
+++ src/starter/02-props/index.tsx
@@ -1,15 +1,17 @@
 type ComponentProps = {
   name: string;
   id: number;
+  children?: React.ReactNode;
 };
 
-function Component({ id, name }: ComponentProps) {
+function Component({ id, name, children }: ComponentProps) {
   return (
     <div>
       <h2>React & Typescript</h2>
       <h2>Props</h2>
       <p>{id}</p>
       <p>{name}</p>
+      <p>{children}</p>
     </div>
   );
 }
